c4d0b57e75d642ec6e54a9cb58f0f6ed36cce652
Anikesh Roy <anikeshroy62040@gmail.com>
2025-10-05 16:51:54 +0530

    Added new project - URL Shortener

--- UrlShortener/index.js
+++ UrlShortener/index.js
@@ -1,8 +1,29 @@
 const express = require('express');
+const connectMongoDB = require('./connect');
+const urlRoute = require('./routes/url');
+const URL = require('./models/url')
 const app = express();
 const PORT = 8000;
 
+connectMongoDB('mongodb://127.0.0.1:27017/short-url')
+    .then(() => console.log('MogoDb is connected'))
+    .catch((err) => console.error('MongoDB connection error:', err));
 
+app.use(express.json());
+
+app.get('/:shortId', async (req, res) => {
+    const shortId = req.params.shortId;
+    const entry = await URL.findOneAndUpdate({
+        shortId
+    }, {
+        $push: {
+            visitHistory: { timestamp: Date.now() },
+        },
+    });
+    res.redirect(entry.redirectUrl);
+})
+
+app.use('/url', urlRoute);
 
 
 app.listen(PORT, () => console.log(`Server is started on port: ${PORT}`));
